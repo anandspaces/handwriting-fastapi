1b7a8c4dbab455e704fca2da3d2090eedbd878f2
anandspaces <anandvaibhaw000@gmail.com>
2026-01-31 00:36:10 +0530

    fix white background

--- demo.py
+++ demo.py
@@ -117,7 +117,7 @@ class Hand(object):
 
         dwg = svgwrite.Drawing(filename=filename)
         dwg.viewbox(width=view_width, height=view_height)
-        dwg.add(dwg.rect(insert=(0, 0), size=(view_width, view_height), fill='white'))
+        # Removed white background rectangle to enable transparent SVG output
 
         initial_coord = np.array([0, -(3*line_height / 4)])
         for offsets, line, color, width in zip(strokes, lines, stroke_colors, stroke_widths):
